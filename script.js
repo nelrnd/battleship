274c7661dc2121ec27de120bd2613c33246919e7
Nel <nel.renaudin@gmail.com>
2023-01-16 14:09:35 +0100

    Start adding game logic

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log('hello');
+/* GAME LOGIC */
+
+import { Player } from './classes/Player.js';
+
+const players = [];
+let turn = 0;
+
+function setupGame() {
+  if (players.length === 0) {
+    players.push(new Player('human'));
+    players.push(new Player('computer'));
+  }
+
+  turn = 0;
+
+  players.forEach((player) => player.board.populateRandomly());
+}
+
+function startGame() {}
+
+function switchTurn() {
+  turn++ % 2;
+}
+
+function getCurrent() {
+  return players[turn];
+}
+
+function getOpponent() {
+  return players[Math.abs(turn - 1)];
+}
